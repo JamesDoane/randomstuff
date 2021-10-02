19913a1d8e33ba156bf7d8ffaa3033304151157e
JamesDoane <ijamesdoaneok@gmail.com>
2021-10-01 20:13:23 -0500

    initial commit

---
+++ freeballin.py
@@ -0,0 +1,33 @@
+import random
+# first_name = input('what is your first name?')
+# last_name = input('what is your last name?')
+# full_name = first_name + " " + last_name
+# print(f"My first name is {first_name} and my last name is {last_name} which means my full name is {full_name}")
+
+# fahrenheit = int(input("what is the temperature in fahrenheit?"))
+# celsius = (fahrenheit-32) * 5 / 9
+# print(f'{fahrenheit} degrees fahrenheit is equal to {celsius} degrees celsius.')
+
+# driving_age = 16
+# user_age = int(input("How old are you? \n"))
+
+# if user_age >= driving_age:
+#     print('You are legally able to drive.')
+# else:
+#     print('You are not old enough to drive yet.')
+
+
+# rand_num = random.randint(0,10)
+
+# if rand_num >= 0 and rand_num <= 2:
+#     print("0 or 1 or 2")
+# elif rand_num >= 3 and rand_num <= 5:
+#     print("3 or 4 or 5")
+# elif rand_num >= 6 and rand_num <= 8:
+#     print("6 or 7 or 8")
+# elif rand_num == 9 or rand_num == 10:
+#     print("9 or 10")
+
+
+
+
